a72ade05d7cf5b4aa1fd002ad5f3569533c0ebe6
VaibhavGuptaEww <vaibhav.gupta@excellentwebworld.in>
2023-04-13 19:15:40 +0530

    Second Commit

--- start/routes.ts
+++ start/routes.ts
@@ -20,6 +20,31 @@
 
 import Route from '@ioc:Adonis/Core/Route'
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+// Route.get('/', async ({ view }) => {
+//   return view.render('welcome')
+// })
+
+
+// Route.get('/posts', async () => {
+//   return "Get all Posts"
+// })
+
+// Route.post('/posts', async () => {
+//   return "Storing Posts"
+// })
+
+// Route.get('/posts/:id', async () => {
+//   return "get Single Post"
+// })
+
+// Route.put('/posts/:id', async () => {
+//   return "Update Post"
+// })
+
+// Route.delete('/posts/:id', async () => {
+//   return "Delete Post"
+// })
+
+Route.resource('/posts', 'PostsController')
+
+
